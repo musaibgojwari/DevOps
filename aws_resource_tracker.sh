3805f29030b7bdd31c5d45e8197273fe170046d5
musaibgojwari <musaibx@gmail.com>
2023-11-22 20:12:21 +0530

    some mods to sh file

--- aws_resource_tracker.sh
+++ aws_resource_tracker.sh
@@ -5,25 +5,24 @@
 # Version: v1
 # This script will report the AWS usage
 
-
-### Here is the list of thing it will give report about ###
+### Here is the list of things it will give report about
 #AWS EC2
 #AWS Buckets
 #AWS LAMBDA
 #AWS IAM-USERS
 
+# Execute this script in the EC2 Instance
+
+set -x
+
 # aws ec2
-echo "printing ec2 instances"
 aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
 
 # list s3 buckets
-echo "printing s3 lists"
 aws s3 ls
 
 # listing lamda functions
-echo "printing lamda functions"
 aws lambda list-functions
 
 # IAM users
-echo "printing IAM users"
 aws iam list-users
